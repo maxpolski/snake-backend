a353f451d84701dae1ec2243e8eaf1777ccf3591
Max Korabelskyi <max_polski@icloud.com>
2021-01-03 17:08:38 +0200

    fix: node-env usage

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-require('dotenv').config()
-import { app } from './src/app'
+process.env.NODE_ENV !== 'PROD' && require('dotenv').config();
+import { app } from './src/app';
 
-app.listen(process.env.PORT || 8888)
+app.listen(process.env.PORT || 8888);
